d024ee1ed0f82db73625dbdfab63f0c9150be002
emptynick <info@cschweppe.de>
2022-01-11 11:35:45 +0000

    Deploying to gh-pages from @ voyager-admin/voyager@4fa1374636328f00d3553e0da50c7e6f33f1427f 🚀

--- assets/contributing_assets.md.0e205354.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as s,a}from"./app.47d88925.js";const m='{"title":"Assets","description":"","frontmatter":{},"headers":[],"relativePath":"contributing/assets.md","lastUpdated":1641899538484}',o={},r=a("",3),i=[r];function c(l,d,n,u,p,_){return s(),t("div",null,i)}var f=e(o,[["render",c]]);export{m as __pageData,f as default};
