f9012a7a426f7b91d9f719a763674ad5739350af
emptynick <info@cschweppe.de>
2022-02-07 11:27:14 +0000

    Deploying to gh-pages from @ voyager-admin/voyager@248b1f6a180938c10e216de6cd4643b62f457b11 🚀

---
+++ assets/overriding_icons.md.0982a711.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,c as s,o as a,a as t}from"./app.b4ee6f2a.js";const _='{"title":"Overriding Icons","description":"","frontmatter":{},"headers":[],"relativePath":"overriding/icons.md","lastUpdated":1644233211973}',o={},p=t("",5),e=[p];function c(i,u,r,l,k,d){return a(),s("div",null,e)}var f=n(o,[["render",c]]);export{_ as __pageData,f as default};
